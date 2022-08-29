d23ede1c09951103bb494318bc260f3508462d07
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-08-29 08:30:59 +0300

    Http logging interceptor added

--- app/src/main/java/com/propil/beertinder/data/remote/network/PunkApiFactory.kt
+++ app/src/main/java/com/propil/beertinder/data/remote/network/PunkApiFactory.kt
@@ -3,6 +3,8 @@ package com.propil.beertinder.data.remote.network
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.create
 
@@ -11,10 +13,21 @@ object PunkApiFactory {
     private const val BASE_URL = "https://api.punkapi.com/v2/"
     private val contentType = "application/json".toMediaType()
 
+    private fun createOkHttpClient(): OkHttpClient {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        return OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    }
+
     private val retrofit = Retrofit.Builder()
         .addConverterFactory(Json.asConverterFactory(contentType))
+        .client(createOkHttpClient())
         .baseUrl(BASE_URL)
         .build()
 
-    val punkApiService = retrofit.create(PunkApiService::class.java)
+    val punkApiService: PunkApiService = retrofit.create(PunkApiService::class.java)
 }
