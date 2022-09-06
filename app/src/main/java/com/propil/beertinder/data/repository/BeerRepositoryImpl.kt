248310dc3d615f4b61c7c3c6afa4e8b9f1704e73
PROPILIL <83790466+PROPILIL@users.noreply.github.com>
2022-09-06 11:57:07 +0300

    fun loadData() added to BeerRepositoryImpl for tests

--- app/src/main/java/com/propil/beertinder/data/repository/BeerRepositoryImpl.kt
+++ app/src/main/java/com/propil/beertinder/data/repository/BeerRepositoryImpl.kt
@@ -3,11 +3,17 @@ package com.propil.beertinder.data.repository
 import android.app.Application
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.propil.beertinder.data.PunkApiPagingSource
 import com.propil.beertinder.data.database.BeerDatabase
 import com.propil.beertinder.data.mapper.BeerMapper
+import com.propil.beertinder.data.remote.model.BeerDto
 import com.propil.beertinder.data.remote.network.PunkApiFactory
 import com.propil.beertinder.domain.logic.BeerRepository
 import com.propil.beertinder.domain.model.Beer
+import kotlinx.coroutines.flow.Flow
 
 class BeerRepositoryImpl(application: Application) : BeerRepository {
 
@@ -50,6 +56,20 @@ class BeerRepositoryImpl(application: Application) : BeerRepository {
     override suspend fun loadBeerDetails(beerId: Int): Beer {
         val response = punkApiService.loadBeerDetails(beerId)
         return mapper.mapResponseToEntity(response)
+    }
+
+    fun loadData(): Flow<PagingData<BeerDto>> {
+        return Pager (
+            config = PagingConfig(
+                pageSize = NETWORK_PAGE_SIZE,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { PunkApiPagingSource(punkApiService)}
+        ).flow
+    }
+
+    companion object {
+        const val NETWORK_PAGE_SIZE = 30
     }
 }
 
